89d3aecb72fc3d91ca43a0a03791929f03e44f6b
danielraysa <daniel@bernofarm.co.id>
2023-06-22 12:10:53 +0700

    update query

--- aset/ajax.php
+++ aset/ajax.php
@@ -21,13 +21,16 @@
     }
 
     if(isset($_POST['datatable'])) {
-        $_sql = "SELECT * FROM daftar_baru db join merk m on db.ID_MERK = m.ID_MERK join lokasi l on db.LOKASI_BARANG = l.ID_LOKASI join ruangan r on db.RUANGAN_BARANG = r.ID_RUANGAN ";
+        $_sql = "SELECT * FROM daftar_baru db LEFT JOIN merk m on db.ID_MERK = m.ID_MERK LEFT JOIN lokasi l on db.LOKASI_BARANG = l.ID_LOKASI LEFT JOIN ruangan r on db.RUANGAN_BARANG = r.ID_RUANGAN ";
         $search = $_POST['search']['value']; // Ambil data yang di ketik user pada textbox pencarian
         $limit = $_POST['length']; // Ambil data limit per page
         $start = $_POST['start']; // Ambil data start
         $sql = mysqli_query($koneksi, $_sql); // Query untuk menghitung seluruh data aset
         $sql_count = mysqli_num_rows($sql); // Hitung data yg ada pada query $sql
-        $query = $_sql." WHERE (KODE_BARANG LIKE '%".$search."%' OR NAMA_BARANG LIKE '%".$search."%')";
+        $query = $_sql;
+        if ($search != '') {
+            $query = $_sql." WHERE (KODE_BARANG LIKE '%".$search."%' OR NAMA_BARANG LIKE '%".$search."%')";
+        }
         /* if($param != null && $value != null){
             $query = "SELECT * FROM daftar_baru WHERE ${param} like '%${value}%' and (KODE_BARANG LIKE '%".$search."%' OR NAMA_BARANG LIKE '%".$search."%')";
         } */
@@ -35,7 +38,8 @@
         $order_field = $_POST['columns'][$order_index]['data']; // Untuk mengambil nama field yg menjadi acuan untuk sorting
         $order_ascdesc = $_POST['order'][0]['dir']; // Untuk menentukan order by "ASC" atau "DESC"
         $order = " ORDER BY ".$order_field." ".$order_ascdesc;
-        $sql_data = mysqli_query($koneksi, $query.$order." LIMIT ".$limit." OFFSET ".$start); // Query untuk data yang akan di tampilkan
+        $full_query = $query.$order." LIMIT ".$limit." OFFSET ".$start;
+        $sql_data = mysqli_query($koneksi, $full_query); // Query untuk data yang akan di tampilkan
         $sql_filter = mysqli_query($koneksi, $query); // Query untuk count jumlah data sesuai dengan filter pada textbox pencarian
         $sql_filter_count = mysqli_num_rows($sql_filter); // Hitung data yg ada pada query $sql_filter
         $data = mysqli_fetch_all($sql_data, MYSQLI_ASSOC); // Untuk mengambil data hasil query menjadi array
